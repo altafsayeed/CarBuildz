ceed807cb428fbae0cf852c6610cacbabab94905
altafsayeed <altafsayeed19@yahoo.com>
2022-12-08 23:22:08 -0800

    login/register pages

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -19,7 +19,7 @@ export const register = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const newUser = newUser({
+    const newUser = new User({
       firstName,
       lastName,
       email,
@@ -33,7 +33,7 @@ export const register = async (req, res) => {
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
