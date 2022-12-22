b5febe5fcb8d20f96f4b1f04cc589b18699f33b1
altafsayeed <altafsayeed19@yahoo.com>
2022-12-22 00:11:53 -0800

    troubleshooting

--- server/index.js
+++ server/index.js
@@ -25,20 +25,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 // app.use(helmet());
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: false,
-//   })
-// );
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
